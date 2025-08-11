79d950cbb5bbc014bbb7393469f54e54c16c7cdd
Panda Jiang <3160104094@zju.edu.cn>
2025-08-11 20:22:28 +0800

    tmp
    Signed-off-by: Panda Jiang <3160104094@zju.edu.cn>

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,144 @@
 #![no_std]
 
+use alloc::{boxed::Box, vec};
+use crab_usb::{
+    Class, Device, Direction, EndpointDescriptor, EndpointType, Interface, Recipient, Request,
+    RequestType, err::USBError,
+};
+use dma_api::{DVec, Direction::Bidirectional, Direction::FromDevice, Direction::ToDevice};
+use log::debug;
+use usb_if::host::ControlSetup;
+
 extern crate alloc;
+
+pub struct Ch341 {
+    usb_device: Device,
+    ctrl_buf: DVec<u8>,
+    status_buf: DVec<u8>,
+    in_ep: Option<EndpointDescriptor>,
+    out_ep: Option<EndpointDescriptor>,
+}
+
+impl Ch341 {
+    pub fn new(d: Device) -> Option<Self> {
+        let desc = &d.descriptor;
+        debug!("device desc: {desc:#?}");
+        if desc.product_id != 0x7523
+            || desc.vendor_id != 0x1a86
+            || !matches!(desc.class(), Class::Communication)
+        {
+            return None;
+        }
+        Some(Self {
+            usb_device: d,
+            ctrl_buf: DVec::zeros(2, 2, Bidirectional).unwrap(),
+            status_buf: DVec::zeros(2, 2, FromDevice).unwrap(),
+            in_ep: None,
+            out_ep: None,
+        })
+    }
+
+    async fn ch341_control_out(
+        interface: &mut Interface,
+        req: u8,
+        value: u16,
+        index: u16,
+        data: &[u8],
+    ) -> Result<usize, USBError> {
+        interface
+            .control_out(
+                ControlSetup {
+                    request_type: RequestType::Vendor,
+                    recipient: Recipient::Device,
+                    request: Request::Other(req),
+                    value,
+                    index,
+                },
+                data,
+            )
+            .await?
+            .await
+            .map_err(|e| USBError::Other(Box::new(e)))
+    }
+    async fn ch341_control_in(
+        interface: &mut Interface,
+        req: u8,
+        value: u16,
+        index: u16,
+        data: &mut [u8],
+    ) -> Result<usize, USBError> {
+        interface
+            .control_in(
+                ControlSetup {
+                    request_type: RequestType::Vendor,
+                    recipient: Recipient::Device,
+                    request: Request::Other(req),
+                    value,
+                    index,
+                },
+                data,
+            )?
+            .await
+            .map_err(|e| USBError::Other(Box::new(e)))
+    }
+
+    async fn ch341_interface_init(&mut self, interface: &mut Interface) -> Result<(), USBError> {
+        // init
+        let mut empty = [0; 0];
+        let mut version = [0; 2];
+        let read = Self::ch341_control_in(interface, 0x5f, 0, 0, &mut version).await?;
+        debug!("read {read}, version {version:#?}");
+        Self::ch341_control_out(interface, 0xa1, 0, 0, &mut empty).await?;
+        Self::ch341_control_out(interface, 0x9a, 0x1312, 0xd982, &mut empty).await?;
+
+        Self::ch341_control_out(interface, 0x9a, 0x0f2c, 0x0007, &mut empty).await?;
+        Self::ch341_control_in(interface, 0x95, 0x2518, 0, &mut version).await?;
+        Self::ch341_control_in(interface, 0x95, 0, 0x0706, &mut version).await?;
+        Self::ch341_control_out(interface, 0x9a, 0x2727, 0, &mut empty).await?;
+
+        Self::ch341_control_out(interface, 0xa1, 0xc39c, 0xb282, &mut empty).await?;
+        Self::ch341_control_out(interface, 0x9a, 0x0f2c, 0x0008, &mut empty).await?;
+        Self::ch341_control_out(interface, 0x9a, 0x2727, 0x0000, &mut empty).await?;
+
+        Ok(())
+    }
+
+    pub async fn init(&mut self) -> Result<(), USBError> {
+        let device = &mut self.usb_device;
+        for config in device.configurations.iter() {
+            debug!("Configuration: {config:?}");
+        }
+        let config = &device.configurations[0];
+        debug!("use configuration 0");
+        let interface = config
+            .interfaces
+            .iter()
+            .find(|interface| {
+                let aif = interface.first_alt_setting();
+                matches!(aif.class(), Class::Communication)
+            })
+            .ok_or(USBError::NotFound)?
+            .first_alt_setting();
+        debug!("Using interface: {interface:?}");
+        let mut interface = device
+            .claim_interface(interface.interface_number, interface.alternate_setting)
+            .await?;
+
+        // below code mainly copies from linux and https://github.com/arceos-usb/arceos_experiment, branch usb-camera-base,
+        //  bear with me, cause nor do I know what these mean
+
+        for ep in &interface.descriptor.endpoints {
+            match (ep.transfer_type, ep.direction) {
+                (EndpointType::Bulk, Direction::In) => self.in_ep = Some(ep.clone()),
+                (EndpointType::Bulk, Direction::Out) => self.out_ep = Some(ep.clone()),
+                _ => debug!("Ignoring endpoint: {ep:?}"),
+            }
+        }
+
+        if self.in_ep.is_none() || self.out_ep.is_none() {
+            return Err(USBError::NotFound);
+        }
+        self.ch341_interface_init(&mut interface).await?;
+        Ok(())
+    }
+}
--- tests/test.rs
+++ tests/test.rs
@@ -7,8 +7,22 @@ extern crate bare_test;
 
 #[bare_test::tests]
 mod tests {
-    use bare_test::println;
-    use log::info;
+    use alloc::{boxed::Box, vec::Vec};
+    use bare_test::{
+        GetIrqConfig,
+        async_std::time,
+        fdt_parser::PciSpace,
+        globals::{PlatformInfoKind, global_val},
+        irq::{IrqHandleResult, IrqInfo, IrqParam},
+        mem::mmu::{iomap, page_size},
+        platform::fdt::GetPciIrqConfig,
+        println,
+    };
+    use core::{pin::Pin, time::Duration};
+    use crab_usb::*;
+    use futures::FutureExt;
+    use log::*;
+    use pcie::*;
 
     #[test]
     fn it_works() {
@@ -17,5 +31,163 @@ mod tests {
         let b = 2;
         assert_eq!(a + b, 4);
         println!("test passed!");
+    }
+    struct XhciInfo {
+        usb: USBHost<Xhci>,
+        irq: Option<IrqInfo>,
+    }
+
+    fn get_usb_host() -> XhciInfo {
+        let PlatformInfoKind::DeviceTree(fdt) = &global_val().platform_info;
+
+        let fdt = fdt.get();
+        let pcie = fdt
+            .find_compatible(&["pci-host-ecam-generic", "brcm,bcm2711-pcie"])
+            .next()
+            .unwrap()
+            .into_pci()
+            .unwrap();
+
+        let mut pcie_regs = alloc::vec![];
+
+        println!("pcie: {}", pcie.node.name);
+
+        for reg in pcie.node.reg().unwrap() {
+            println!(
+                "pcie reg: {:#x}, bus: {:#x}",
+                reg.address, reg.child_bus_address
+            );
+            let size = reg.size.unwrap_or_default().align_up(0x1000);
+
+            pcie_regs.push(iomap((reg.address as usize).into(), size));
+        }
+
+        let mut bar_alloc = SimpleBarAllocator::default();
+
+        for range in pcie.ranges().unwrap() {
+            info!("pcie range: {range:?}");
+
+            match range.space {
+                PciSpace::Memory32 => bar_alloc.set_mem32(range.cpu_address as _, range.size as _),
+                PciSpace::Memory64 => bar_alloc.set_mem64(range.cpu_address, range.size),
+                _ => {}
+            }
+        }
+
+        let base_vaddr = pcie_regs[0];
+
+        info!("Init PCIE @{base_vaddr:?}");
+
+        let mut root = RootComplexGeneric::new(base_vaddr);
+
+        // for elem in root.enumerate_keep_bar(None) {
+        for elem in root.enumerate(None, Some(bar_alloc)) {
+            debug!("PCI {elem}");
+
+            if let Header::Endpoint(mut ep) = elem.header {
+                ep.update_command(elem.root, |mut cmd| {
+                    cmd.remove(CommandRegister::INTERRUPT_DISABLE);
+                    cmd | CommandRegister::IO_ENABLE
+                        | CommandRegister::MEMORY_ENABLE
+                        | CommandRegister::BUS_MASTER_ENABLE
+                });
+
+                for cap in &mut ep.capabilities {
+                    match cap {
+                        PciCapability::Msi(msi_capability) => {
+                            msi_capability.set_enabled(false, &mut *elem.root);
+                        }
+                        PciCapability::MsiX(msix_capability) => {
+                            msix_capability.set_enabled(false, &mut *elem.root);
+                        }
+                        _ => {}
+                    }
+                }
+
+                println!("irq_pin {:?}, {:?}", ep.interrupt_pin, ep.interrupt_line);
+
+                if matches!(ep.device_type(), DeviceType::UsbController) {
+                    let bar_addr;
+                    let mut bar_size;
+                    match ep.bar {
+                        pcie::BarVec::Memory32(bar_vec_t) => {
+                            let bar0 = bar_vec_t[0].as_ref().unwrap();
+                            bar_addr = bar0.address as usize;
+                            bar_size = bar0.size as usize;
+                        }
+                        pcie::BarVec::Memory64(bar_vec_t) => {
+                            let bar0 = bar_vec_t[0].as_ref().unwrap();
+                            bar_addr = bar0.address as usize;
+                            bar_size = bar0.size as usize;
+                        }
+                        pcie::BarVec::Io(_bar_vec_t) => todo!(),
+                    };
+
+                    println!("bar0: {:#x}", bar_addr);
+                    println!("bar0 size: {:#x}", bar_size);
+                    bar_size = bar_size.align_up(0x1000);
+                    println!("bar0 size algin: {:#x}", bar_size);
+
+                    let addr = iomap(bar_addr.into(), bar_size);
+                    trace!("pin {:?}", ep.interrupt_pin);
+
+                    let irq = pcie.child_irq_info(
+                        ep.address.bus(),
+                        ep.address.device(),
+                        ep.address.function(),
+                        ep.interrupt_pin,
+                    );
+
+                    println!("irq: {irq:?}");
+
+                    return XhciInfo {
+                        usb: USBHost::new(addr),
+                        irq,
+                    };
+                }
+            }
+        }
+
+        for node in fdt.all_nodes() {
+            if node.compatibles().any(|c| c.contains("xhci")) {
+                println!("usb node: {}", node.name);
+                let regs = node.reg().unwrap().collect::<Vec<_>>();
+                println!("usb regs: {:?}", regs);
+
+                let addr = iomap(
+                    (regs[0].address as usize).into(),
+                    regs[0].size.unwrap_or(0x1000),
+                );
+
+                let irq = node.irq_info();
+
+                return XhciInfo {
+                    usb: USBHost::new(addr),
+                    irq,
+                };
+            }
+        }
+
+        panic!("no xhci found");
+    }
+
+    fn register_irq(irq: IrqInfo, host: &mut Pin<Box<USBHost<Xhci>>>) {
+        let ptr: *mut USBHost<Xhci> = host.as_mut().get_mut() as *mut _;
+
+        for one in &irq.cfgs {
+            IrqParam {
+                intc: irq.irq_parent,
+                cfg: one.clone(),
+            }
+            .register_builder({
+                move |_irq| {
+                    unsafe {
+                        (&mut *ptr).handle_irq();
+                    }
+                    IrqHandleResult::Handled
+                }
+            })
+            .register();
+        }
     }
 }
